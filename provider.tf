f1a63de8587d605831b7feb40f1e95a5a3c6195d
Luís Felipe Amengual Tatsch <lfelipe_at@yahoo.com.br>
2024-05-13 21:14:07 -0300

    Provider

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
 # Define the MongoDB Atlas and AWS Providers
 terraform {
-    backend "s3" {
-    bucket = "piklesfastfood"
-    key    = "BDTerraformMongo/terraform.tfstate"
-    region = "us-east-1"
-  }
   required_providers {
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
@@ -13,5 +8,5 @@ terraform {
       source = "hashicorp/aws"
     }
   }
-  required_version = ">= 1.2"
+  required_version = ">= 0.13"
 }
